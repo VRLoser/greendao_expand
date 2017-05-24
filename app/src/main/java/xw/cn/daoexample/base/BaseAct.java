088590e9710a361c4deb05e8b5410b1a573bda67
VRLoser <vrloser0226@163.com>
2017-05-24 19:21:57 +0800

    test

---
+++ app/src/main/java/xw/cn/daoexample/base/BaseAct.java
@@ -0,0 +1,18 @@
+package xw.cn.daoexample.base;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Xw on 2017/5/24.
+ */
+
+public class BaseAct extends AppCompatActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
---
+++ app/src/main/java/xw/cn/daoexample/base/MyApp.java
@@ -0,0 +1,15 @@
+package xw.cn.daoexample.base;
+
+import android.app.Application;
+
+/**
+ * Created by Xw on 2017/5/24.
+ */
+
+public class MyApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
